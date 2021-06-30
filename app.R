07f7072d8d6e9760657c71a884dad105aeaba38a
Abdullahi <shinamuh@yahoo.com>
2021-06-30 08:03:57 +0000

    read data into app and check that it loads successfully.

--- app.R
+++ app.R
@@ -1,4 +1,13 @@
 library(shiny)
+
+#Load data
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+#print(str(bcl)) #This code checks that app can successfully read the data
+
+#ui
 ui <- fluidPage()
+
+#server
 server <- function(input, output) {}
+
 shinyApp(ui = ui, server = server)
