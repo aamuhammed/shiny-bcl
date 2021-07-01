94a78ea5785edbc8d2d66f4fcab49458c9ceec3d
Abdullahi <shinamuh@yahoo.com>
2021-07-01 07:26:40 +0000

    Add a title

--- app.R
+++ app.R
@@ -5,13 +5,8 @@ bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 #print(str(bcl)) #This code checks that app can successfully read the data
 
 #ui
-ui <- fluidPage(h1("My app"),
-                "BC",
-                "Liquor",
-                br(),
-                "Store",
-                strong("prices"),
-                div("this is blue", style = "color: blue;")
+ui <- fluidPage(titlePanel("BC Liquor Store prices")
+                
                 )
 
 #server
