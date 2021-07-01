78a200f1a674d53b4ff3691c60f27cf868ad318a
Abdullahi <shinamuh@yahoo.com>
2021-07-01 07:00:02 +0000

    Add plain text to the UI

--- app.R
+++ app.R
@@ -5,7 +5,7 @@ bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 #print(str(bcl)) #This code checks that app can successfully read the data
 
 #ui
-ui <- fluidPage()
+ui <- fluidPage('BC Liquor' ,'Store Prices')
 
 #server
 server <- function(input, output) {}
