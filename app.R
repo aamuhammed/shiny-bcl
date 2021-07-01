6d8efdfef9a3dbec990949429a2ec9da8e18f166
Abdullahi <shinamuh@yahoo.com>
2021-07-01 07:22:15 +0000

    Add formatted text and other HTML elements

--- app.R
+++ app.R
@@ -5,7 +5,14 @@ bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 #print(str(bcl)) #This code checks that app can successfully read the data
 
 #ui
-ui <- fluidPage('BC Liquor' ,'Store Prices')
+ui <- fluidPage(h1("My app"),
+                "BC",
+                "Liquor",
+                br(),
+                "Store",
+                strong("prices"),
+                div("this is blue", style = "color: blue;")
+                )
 
 #server
 server <- function(input, output) {}
