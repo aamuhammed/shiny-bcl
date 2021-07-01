bc317bf345b04ad705b480a3181ed7bdcc903868
Abdullahi <shinamuh@yahoo.com>
2021-07-01 07:37:44 +0000

    Add a layout

--- app.R
+++ app.R
@@ -5,8 +5,11 @@ bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 #print(str(bcl)) #This code checks that app can successfully read the data
 
 #ui
-ui <- fluidPage(titlePanel("BC Liquor Store prices")
-                
+ui <- fluidPage(titlePanel("BC Liquor Store prices"),
+                sidebarLayout(
+                  sidebarPanel("our inputs will go here"),
+                  mainPanel("the results will go here")
+                )
                 )
 
 #server
