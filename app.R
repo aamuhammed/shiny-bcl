d91efa10c24223e73934830ec20f3b5667719b36
Abdullahi <shinamuh@yahoo.com>
2021-07-02 07:22:06 +0000

    Add input for price, product type & country.

--- app.R
+++ app.R
@@ -8,7 +8,13 @@ bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 ui <- fluidPage(titlePanel("BC Liquor Store prices"),
                 
                 sidebarLayout(
-                  sidebarPanel("our inputs will go here"),
+                  sidebarPanel(sliderInput("priceInput", "Price", min = 0, max = 100,
+                                           value = c(25, 40), pre = "$"),
+                               radioButtons("typeInput", "Product type",
+                                            choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                                            selected = "WINE"),
+                               selectInput("countryInput", "Country",
+                                           choices = c("CANADA", "FRANCE", "ITALY"))),
                   mainPanel("the results will go here")
                 )
                 
