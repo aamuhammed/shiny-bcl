c72c6cfc9118b46bd766645476dcd9f42ec16c5e
Abdullahi <shinamuh@yahoo.com>
2021-07-05 10:00:59 +0000

    Reduce code duplication using reactive variables.

--- app.R
+++ app.R
@@ -38,29 +38,23 @@ ui <- fluidPage(titlePanel("BC Liquor Store prices"),
 
 #server
 server <- function(input, output) {
-  output$coolplot <- renderPlot({
-    filtered <-
-      bcl %>%
-      filter(
-        Price >= input$priceInput[1],
-        Price <= input$priceInput[2],
-        Type == input$typeInput,
-        Country == input$countryInput
-      )
-    
-    ggplot(filtered, aes(Alcohol_Content)) +
-      geom_histogram()
-  })
-  output$results <- renderTable({
-    filtered <-
-      bcl %>%
+  filtered <- reactive({
+    bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
              Type == input$typeInput,
              Country == input$countryInput
       )
-    filtered
   })
+  
+  output$coolplot <- renderPlot({
+    ggplot(filtered(), aes(Alcohol_Content)) +
+      geom_histogram()
+  })
+  output$results <- renderTable({
+    filtered()
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
