9723ea648ce6f514ea97014e2a082bc5379049f1
Abdullahi <shinamuh@yahoo.com>
2021-07-07 08:16:26 +0000

    Use uiOutput() in our app to populate the countries

--- app.R
+++ app.R
@@ -7,38 +7,37 @@ bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 #print(str(bcl)) #This code checks that app can successfully read the data
 
 #ui
-ui <- fluidPage(titlePanel("BC Liquor Store prices"),
-                
-                sidebarLayout(
-                  sidebarPanel(
-                    sliderInput(
-                      "priceInput",
-                      "Price",
-                      min = 0,
-                      max = 100,
-                      value = c(25, 40),
-                      pre = "$"
-                    ),
-                    radioButtons(
-                      "typeInput",
-                      "Product type",
-                      choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                      selected = "WINE"
-                    ),
-                    selectInput(
-                      "countryInput",
-                      "Country",
-                      choices = c("CANADA", "FRANCE", "ITALY")
-                    )
-                  ),
-                  mainPanel(plotOutput("coolplot"),
-                            br(), br(),
-                            tableOutput("results"))
-                ))
+ui <- fluidPage(
+  titlePanel("BC Liquor Store prices"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
+      radioButtons("typeInput", "Product type",
+                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                   selected = "WINE"),
+      uiOutput("countryOutput")
+    ),
+    mainPanel(
+      plotOutput("coolplot"),
+      br(), br(),
+      tableOutput("results")
+    )
+  )
+)
 
 #server
 server <- function(input, output) {
+  output$countryOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
+  })  
+  
   filtered <- reactive({
+    if (is.null(input$countryInput)) {
+      return(NULL)
+    }    
+    
     bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
@@ -48,14 +47,16 @@ server <- function(input, output) {
   })
   
   output$coolplot <- renderPlot({
+    if (is.null(filtered())) {
+      return()
+    }
     ggplot(filtered(), aes(Alcohol_Content)) +
       geom_histogram()
   })
+  
   output$results <- renderTable({
     filtered()
   })
-  
 }
 
 shinyApp(ui = ui, server = server)
-
